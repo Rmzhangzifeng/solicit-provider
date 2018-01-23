0ebea7d2c678694b16b889088ddc50903e143bb8
快乐 <1305370592@qq.com>
2018-01-23 16:04:50 +0800

    xfc

--- src/main/java/org/maker/SolicitProviderApplication.java
+++ src/main/java/org/maker/SolicitProviderApplication.java
@@ -13,11 +13,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableTransactionManagement
 @ComponentScan(basePackages = {"org.maker"})
 @MapperScan("org.maker.dao")
 public class SolicitProviderApplication {
